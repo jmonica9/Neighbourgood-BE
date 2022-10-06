3933c46a97a43b574930f19d49d3601ef16b7ad2
jmonica9 <jmzmonica99@gmail.com>
2022-10-07 07:39:10 +0800

    passport with jwt cookies persistable user info

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -1,6 +1,11 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const localStrategy = require("passport-local").Strategy;
+const JWTStrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+
+const secret = "secretcode";
+const passport = require("passport");
 
 module.exports = function (passport) {
   passport.use(
@@ -20,6 +25,7 @@ module.exports = function (passport) {
     })
   );
 
+  //check for session
   passport.serializeUser((user, cb) => {
     cb(null, user.id);
   });
@@ -32,3 +38,45 @@ module.exports = function (passport) {
     });
   });
 };
+const cookieExtractor = function (req) {
+  var token = null;
+  if (req && req.cookies) {
+    token = req.cookies["jwt"];
+  }
+  return token;
+};
+//check JWT token here instead
+//from front-end you send the JWT token in the request header
+// jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+const options = {
+  jwtFromRequest: cookieExtractor,
+  secretOrKey: secret,
+  passReqToCallback: true, //<= Important, so that the verify function can accept the req param ie verify(req,payload,done)
+  //so that ur req.user can be accurately recorded bc using cookies will not be using passport sessions
+};
+
+passport.use(
+  "jwt",
+  new JWTStrategy(options, async (req, jwt_payload, done) => {
+    try {
+      console.log(jwt_payload);
+      let user = await User.findOne({
+        _id: jwt_payload.id,
+        // where: {
+        //   _id: jwt_payload.id,
+        // },
+      });
+
+      if (user) {
+        req.user = user; //<= so that any route that has the passport.authenticate("jwt", {session: false}) middleware will receive req.user upon successful authentication
+        console.log("User found in DB", user);
+        return done(null, user);
+      } else {
+        console.log("User not in db");
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const cookieParser = require("cookie-parser");
 const bcrypt = require("bcryptjs");
 const session = require("express-session");
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
 //import routers
 const UserRouter = require("./routers/userRouter");
@@ -19,6 +20,7 @@ const UserRouter = require("./routers/userRouter");
 const UserController = require("./controllers/userController");
 //import models here
 const userModel = require("./models/userModel");
+const { create } = require("./models/userModel");
 
 // Middleware
 app.use(bodyParser.json());
@@ -55,16 +57,48 @@ app.use(express.json());
 //initialise routes here
 app.use("/users", userRouter);
 
+//JWT token
+const maxAge = 3 * 24 * 60 * 60;
+//3 days
+const createToken = (id) => {
+  return jwt.sign({ id }, "secretcode", {
+    expiresIn: maxAge,
+  });
+};
+
 // Routes
 app.post("/login", (req, res, next) => {
   passport.authenticate("local", (err, user, info) => {
-    if (err) throw err;
-    if (!user) res.send("No User Exists");
+    if (err) {
+      console.log(err);
+      return res.status(500).json;
+    } else if (!user) return res.send("No User Exists");
     else {
       req.logIn(user, (err) => {
-        if (err) throw err;
-        res.send("Successfully Authenticated");
-        console.log(req.user);
+        if (err) {
+          console.log(err);
+          return res.status(500).json;
+        }
+        // Successfully Authenticated
+        // create token for the logged in user
+        const token = createToken(req.user._id);
+        //send cookie to client browser
+        res.cookie("jwt", token, { httpOnly: false, maxAge: maxAge * 1000 });
+        console.log(req.user, "req user");
+
+        return res.json({
+          success: true,
+          token: "JWT " + token,
+          user: req.user,
+        });
+        /*other way to create token & send status*/
+        //   // const token = jwt.sign({ id: userInfo.id }, jwtSecret.secret, {
+        //   //   expiresIn: 60 * 24,
+        //   // });
+        // res.status(200).json({ user: user._id, status: true });
+        //     return res
+        //       .status(200)
+        //       .send({ auth: true, token, message: "User found and logged in" });
       });
     }
   })(req, res, next);
@@ -73,10 +107,14 @@ app.post("/login", (req, res, next) => {
 app.post("/register", (req, res) => {
   console.log("register route!", req.body);
   userModel.findOne({ username: req.body.username }, async (err, doc) => {
-    if (err) throw err;
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+      return;
+    }
     if (doc) {
       console.log(doc);
-      res.send("User Already Exists");
+      return res.send("User Already Exists");
     }
     if (!doc) {
       console.log("doc dont exist");
@@ -94,11 +132,41 @@ app.post("/register", (req, res) => {
         { $setOnInsert: { password: hashedPassword } },
         { upsert: true, new: true }
       );
+      // //create token for the logged in user
+      // const token = createToken(user._id);
+      // //send cookie
+      // res.cookie("jwt", token, {
+      //   withCredentials: true,
+      //   httpOnly: false,
+      //   maxAge: maxAge * 1000,
+      // });
+      // console.log("made cookie");
       return res.json(user);
       // res.send("User Created");
     }
   });
 });
+
+app.get("/myUser", (req, res) => {
+  res.send(req.user); // The req.user stores the entire user that has been authenticated inside of it.
+});
+
+//test JWT token from cookie
+app.get(
+  "/protectedbyjwt",
+  // (req, res) => {
+  //   console.log("jwt route protected", req.user);
+  //   passport.authenticate("jwt", { session: false }, (req, res, next) => {
+  //     console.log(req.user, "hi");
+  //     return res.json(req.user);
+  //   });
+  // }
+  passport.authenticate("jwt", { session: false }),
+  (req, res, next) => {
+    console.log("jwt route protected");
+    res.json(req.user);
+  }
+);
 
 //----------------------------------------- END OF ROUTES---------------------------------------------------
 
