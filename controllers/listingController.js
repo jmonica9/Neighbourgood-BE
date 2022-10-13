baa0e3f623aa5e6cd668004a216cdaa0faec7ddb
jmonica9 <jmzmonica99@gmail.com>
2022-10-13 18:46:29 +0800

    lobby requests controller done

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -41,6 +41,52 @@ class ListingController extends BaseController {
       return res.status(400).json({ error: true, msg: err });
     }
   };
+
+  getTypeListings = async (req, res) => {
+    console.log("get type listings route");
+    console.log(req.params, "req params");
+    const { type } = req.params;
+    let requestedtype = type;
+    try {
+      const listings = await this.model.find({ type: requestedtype });
+      return res.json(listings);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  };
+  getMyTypeListings = async (req, res) => {
+    console.log("get my type listings route");
+    console.log(req.params, "req params");
+    const { type, userId } = req.params;
+    let requestedtype = type;
+    let requestedUserId = userId;
+    try {
+      const listings = await this.model.find({
+        type: requestedtype,
+        userId: requestedUserId,
+      });
+      return res.json(listings);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  };
+
+  getMyTypeWatchlist = async (req, res) => {
+    console.log("get my type listings route");
+    console.log(req.params, "req params");
+    const { type, userId } = req.params;
+    let requestedtype = type;
+    let requestedUserId = userId;
+    try {
+      const listings = await this.model.find({
+        type: requestedtype,
+        requestorIds: requestedUserId,
+      });
+      return res.json(listings);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  };
 }
 
 module.exports = ListingController;
--- routers/listingRouter.js
+++ routers/listingRouter.js
@@ -8,6 +8,12 @@ class ListingRouter {
   routes() {
     router.get("/", this.controller.getAll);
     router.post("/", this.controller.insertOne);
+    //all listings for one type where user is inside the requestorIds
+    router.get("/:type/:userId/watchlist", this.controller.getMyTypeWatchlist);
+    // all listings for one type
+    router.get("/:type", this.controller.getTypeListings);
+    //my own listings according to type
+    router.get("/:type/:userId", this.controller.getMyTypeListings);
 
     return router;
   }
